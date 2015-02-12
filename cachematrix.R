dbeee295c8db949153a870c8dcf2f0df0955c692
Paolo Midali <pmbg@operamail.com>
2015-02-12 08:14:08 +0100

    improved comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,15 @@
 ## makeCacheMatrix returns a list whose elements are functions
-## it contains both the original matrix, m, and its inverse, if calculated, in s
-## s contains the solve value, if set
-## set() is not used
-## get() returns the matrix stored when makeCacheMatrix is called
-## setsolve stores the value of the inverse matrix in s
-## getsolve returns s, which can be null or the calculated value, if available
+## it contains both the original matrix, x, and its inverse, if calculated, in s
+## - s contains the cached value, if already set
+## - set() is not actually used by cacheSolve; 
+##     it can be used from outside to replace the matrix x with another one, in that case the cached value
+##     will be cleaned at it will be obsolete
+## - get() returns the original matrix (x) stored when makeCacheMatrix is called
+## - setsolve caches the value of the solve value in s
+## - getsolve returns the cached value, which can be null or valid
+## - a list is returned, containing the four functions defined. Accessing the items 
+##     (e.g. {cm  <- makeCacheMatrix(m); cm$get() } ) will actually call the specified function.
+##     In this case, the get() function will be called, and the original matrix will be returned
 
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
@@ -21,11 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve takes a list created by makeCacheMatrix as input
-## if the inverse matrix has not yet been calculated, x$getsolve returns null;
-##   in this case the value is calculated calling solve(), and cached calling setsolve()
-## if the inverse has been calculated and set in the list created with makeCacheMatrix,
-## it will be available, and solve() won't be called anymore
+## cacheSolve takes an object (list) created by makeCacheMatrix as input
+## - if the inverse matrix has not yet been calculated, x$getsolve returns null;
+##     in this case the value is calculated calling solve(), and cached calling x's setsolve()
+## - if the inverse has been calculated and set in the list created with makeCacheMatrix,
+##     it will be available in next calls to x$getsolve(), and solve() won't be called anymore
+## - either the cached or the calculated value (m) is returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
